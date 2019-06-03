ddd29194cbd86ca754b2218ec109ab5b1363e71a
Danil Danilchenko <danil.d@ibearsoft.com>
2019-06-03 16:44:07 +0700

    Added unit-testing

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestMethod2() {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestMethod3() {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestMethod4() {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestMethod5() {
+            Assert.IsTrue(true);
+        }
+    }
+}
